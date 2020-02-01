7850e2bd20df1df09e600bc9a68693c4aef09582
YONGJAE NAM <34469792+YJNN@users.noreply.github.com>
2020-02-01 19:55:50 +0900

    Update mainmenu.java

--- app/src/main/java/com/example/user/silver/mainmenu.java
+++ app/src/main/java/com/example/user/silver/mainmenu.java
@@ -12,35 +12,26 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class mainmenu extends AppCompatActivity {
-
     RelativeLayout layout;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainmenu);
         layout = (RelativeLayout) findViewById(R.id.mainmenu);
-
-        // Intent intent = getIntent();
-        //String username = intent.getStringExtra(login.USER_NAME);
-
-        //노래교실버튼
+        
         Button musicshool = (Button) findViewById(R.id.musicshool);
         musicshool.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(mainmenu.this, musicschool.class);
                 startActivity(intent);
             }
         });
 
-
-        //게이트볼 버튼
         Button gateball = (Button) findViewById(R.id.gateball);
         gateball.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(mainmenu.this, gateball.class);
                 startActivity(intent);
             }
@@ -51,13 +42,11 @@ public class mainmenu extends AppCompatActivity {
         dancesports.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(mainmenu.this, dancesports.class);
                 startActivity(intent);
             }
         });
 
-        //computersports버튼
         Button computerschool = (Button) findViewById(R.id.computerschool);
         computerschool.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,23 +57,19 @@ public class mainmenu extends AppCompatActivity {
             }
         });
 
-        //자유게시판 버튼
         Button free = (Button) findViewById(R.id.free);
         free.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(mainmenu.this, qna.class);
                 startActivity(intent);
             }
         });
 
-        //qna버튼
         Button qna = (Button) findViewById(R.id.qna);
         qna.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(mainmenu.this, Silvermarket.class);
                 startActivity(intent);
             }
